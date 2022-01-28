9a471a94bb27d9193dc718834bd200498dd5bf89
Farkas Ükös <ukosfarkas@gmail.com>
2022-01-28 11:48:08 +0100

    sdf

--- Farkas_munkái.py
+++ Farkas_munkái.py
@@ -1,3 +1,5 @@
+from main import Tanulo
+
 def feladat1(lista):
     print("1) Hány diák tanul az osztályban?")
     print(len(lista))
@@ -7,7 +9,7 @@ def feladat4(lista):
     print("4) Hány olyan diák van, akiknek több mint 1 testvére van!")
     mennyi = 0
     for sor in lista:
-        if sor.testverszama > 1:
+        if int(sor.testverszama) > 1:
             mennyi += 1 
     print(mennyi)        
     print()
@@ -16,9 +18,16 @@ def feladat7(lista):
     print("7) Gyűjtse ki azon diákok nevét, akiknek több mint 2 testvérük van!")
     vissza = []
     for sor in lista:
-        if sor.testverszam > 2:
+        if int(sor.testverszama) > 2:
             vissza.append(sor.nev)
             print(sor.nev)
 
 def feladat10(lista):
     print("10) Hány diák tanul, az egyes angol csoportban?")
+
+
+
+feladat1(Tanulo.lista)
+feladat4(Tanulo.lista)
+feladat7(Tanulo.lista)
+feladat10(Tanulo.lista)
--- main.py
+++ main.py
@@ -16,9 +16,3 @@ with open("J.txt", "r", encoding="utf8") as f:
     for sor in f:
         s = sor.strip().split(";")
         t = Tanulo(int(s[0]), s[1], s[2], s[3], s[4], s[5], int(s[6]), int(s[7]))
-
-
-for t in Tanulo.lista:
-    print(t.nev)
-
-print(len(Tanulo.lista))
