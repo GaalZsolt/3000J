ea94b5de7008ad65edfc8740780c511eaa88ec69
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-01-28 11:42:27 +0100

    feladat2 elméletileg működik

--- Bence.py
+++ Bence.py
@@ -1,15 +1,13 @@
 from main import Tanulo
 
 
-#függvények
-
-def feladat1(lista):
-    
-
-
-
-
-
+def feladat2(lista):
+    print('Hány fiú tanul az osztályban?')
+    fiukszama = 0
+    for t in Tanulo.lista:
+        if t.nem == 'F':
+            fiukszama+=1
+    print(fiukszama)
 
 
 
@@ -17,22 +15,10 @@ def feladat1(lista):
 
 
 
-
-
-
-
-# program  
-
-with open("input.txt", "r", encoding="utf8") as f:
-    for sor in f:
-        s = sor.strip().split(";")
-        t = Tanulo(int(s[0]), s[1], s[2], s[3], s[4], s[5], int(s[6]), int(s[7]))
-
-
-for t in Tanulo.lista:
-    print(t.nev)
 
 print(len(Tanulo.lista))
 
-print(feladat1(lista))
+feladat2(Tanulo.lista)
+
+
 
