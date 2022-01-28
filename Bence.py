75fa9ade091ed295cc938c496320c28f52bd4d36
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-01-28 13:04:37 +0100

    26. complete

--- Bence.py
+++ Bence.py
@@ -8,12 +8,14 @@ def feladat2(lista):
         if t.nem == 'F':
             fiukszama+=1
     print(fiukszama)
+    print()
 
 def feladat5(lista):
     print('5) Gyűjtse ki azon diákok nevét, akiknek több mint 1 testvérük van!')
     for t in Tanulo.lista:
         if t.testverszama > 1:
             print(t.nev)
+    print()
 
 def feladat8(lista):
     print('8) Hány olyan diák van, akik a 2. idegen nyelvként a németet tanulják!')
@@ -22,6 +24,7 @@ def feladat8(lista):
         if t.mnyelv == 'német':
             nemetesekszama+=1
     print(nemetesekszama)
+    print()
 
 def feladat11(lista):
     print('11) Hány diák tanul, a kettes angol csoportban?')
@@ -30,6 +33,7 @@ def feladat11(lista):
         if t.acsop == '2. Bán':
             kettesangol+=1
     print(kettesangol)
+    print()
 
 def feladat14(lista):
     print('14) Hány lány diák tanul, az alfa matematika csoportban?')
@@ -38,6 +42,7 @@ def feladat14(lista):
         if t.nem == 'L' and t.micsop == 'alfa':
             lanyokszama+=1
     print(lanyokszama)
+    print()
 
 def feladat17(lista):
     print('17) Hány fiú diák tanul, a beta matematika csoportban?')
@@ -46,6 +51,51 @@ def feladat17(lista):
         if t.nem == 'F' and t.micsop == 'beta':
             fiukszama+=1
     print(fiukszama)
+    print()
+
+def feladat20(lista):
+    print('20) Van-e olyan diák, aki a 2. idegen nyelvként spanyolt tanul?')
+    vane = False
+    i = 0
+    while not vane  and i < len(Tanulo.lista):
+        if Tanulo.lista[i].mnyelv == 'spanyol':
+            vane = True
+        i+=1
+    print(vane)
+    print() 
+
+def feladat23(lista):
+    print('23) Gyűjtse ki azon lány diákok nevét, akik az egyes vagy kettes angol csoportban vannak!')
+    for t in Tanulo.lista:
+        if t.acsop == '2. Bán' or t.acsop == '1. Sió':
+            print(t.nev)
+    print()
+
+
+def feladat26(lista):
+    print('26) Dári Dóra hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak.')
+    for ta in Tanulo.lista:
+        if ta.nev == 'Dári Dóra':
+            for tb in Tanulo.lista:
+                if  tb.acsop == ta.acsop and tb.nev != 'Dári Dóra':
+                    print(tb.nev)
+    print()
+
+def feladat29(lista):
+    print('29) Citad Ella hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak. ')
+    for t in Tanulo.lista:
+        pass
+
+
+"""template
+
+def feladat(lista):
+    print('')
+
+    for t in Tanulo.lista:
+        if
+
+"""
 
 
 feladat2(Tanulo.lista)
@@ -60,6 +110,11 @@ feladat14(Tanulo.lista)
 
 feladat17(Tanulo.lista)
 
+feladat20(Tanulo.lista)
+
+feladat23(Tanulo.lista)
+
+feladat26(Tanulo.lista)
 
 
 
